4a97483783dde2feb78a08b3605517e5de32067a
Diganta Das <diganta.das@goevive.com>
2022-09-16 17:23:12 +0530

    Grid system fixed

--- benefits-mfe/src/Benefits.js
+++ benefits-mfe/src/Benefits.js
@@ -5,32 +5,40 @@ import {CardBase, Grid, TextBody, TextHeader} from "@evive/ui-kit";
 
 const Benefits = () => (
 	<div>
-		<div> Benefits </div>
-		<Grid colSpan={3}>
+		<div> Benefits</div>
+		<Grid container>
+			<Grid colSpan={3}>
 			<CardBase>
 				<div>
 					<TextHeader>Card 1</TextHeader>
 					<TextBody>Description</TextBody>
 				</div>
 			</CardBase>
+			</Grid>
+			<Grid colSpan={3}>
 			<CardBase>
 				<div>
 					<TextHeader>Card 1</TextHeader>
 					<TextBody>Description</TextBody>
 				</div>
 			</CardBase>
+			</Grid>
+			<Grid colSpan={3}>
 			<CardBase>
 				<div>
 					<TextHeader>Card 1</TextHeader>
 					<TextBody>Description</TextBody>
 				</div>
 			</CardBase>
+			</Grid>
+			<Grid colSpan={3}>
 			<CardBase>
 				<div>
 					<TextHeader>Card 1</TextHeader>
 					<TextBody>Description</TextBody>
 				</div>
 			</CardBase>
+			</Grid>
 		</Grid>
 	</div>
 );
