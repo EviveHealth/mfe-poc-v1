17f230de55e76f03cc2388feb8d845ba45b06034
vaibhavg15 <vaibhav.goswami@goevive.com>
2022-09-16 17:30:04 +0530

    user bar

--- evive-container/src/Contianer.jsx
+++ evive-container/src/Contianer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import Dashboard from 'DashboardRemote/dashboard'
-import {PageLayout} from "@evive/ui-kit";
+import {PageLayout, SIZE, TextHeader} from "@evive/ui-kit";
+
+import './container.css';
 
 export const Container = () => {
 	const getUserBar = () => (
-		<div>
-			UserBar
+		<div className='container-user-bar'>
+			<PageLayout>
+				<div className='container-user-bar-inner'>
+					<TextHeader size={SIZE.XXL}>Evive</TextHeader>
+					<TextHeader size={SIZE.XXL}>Logout</TextHeader>
+				</div>
+			</PageLayout>
 		</div>
 	)
 	return (
