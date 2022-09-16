a09969f0b5864754687fd5f7e7ad3010dbb65788
Diganta Das <diganta.das@goevive.com>
2022-09-16 18:05:33 +0530

    Removed proxy from dashboard

--- dashboard-mfe/webpack.config.js
+++ dashboard-mfe/webpack.config.js
@@ -83,29 +83,29 @@ module.exports = (env) => ({
 		historyApiFallback: true,
 		open: false,
 		port: 8081,
-		proxy: {
-			'/planchoice/api': {
-				target: 'https://pc.dev.myevive.me',
-				secure: true,
-				changeOrigin: false,
-				headers: {
-					Connection: 'keep-alive',
-					Host: 'pc.dev.myevive.me',
-				},
-				bypass(
-					req, res, proxyOptions,
-				) {
-					console.log(
-						'PROXY', req.method, req.url,
-					);
-					// if (req.headers.accept.indexOf('html') !== -1) {
-					//   console.log('Skipping proxy for browser request.');
-					//   return '/index.html';
-					// }
-				},
-			},
-			hot: true,
-		},
+	// 	proxy: {
+	// 		'/planchoice/api': {
+	// 			target: 'https://pc.dev.myevive.me',
+	// 			secure: true,
+	// 			changeOrigin: false,
+	// 			headers: {
+	// 				Connection: 'keep-alive',
+	// 				Host: 'pc.dev.myevive.me',
+	// 			},
+	// 			bypass(
+	// 				req, res, proxyOptions,
+	// 			) {
+	// 				console.log(
+	// 					'PROXY', req.method, req.url,
+	// 				);
+	// 				// if (req.headers.accept.indexOf('html') !== -1) {
+	// 				//   console.log('Skipping proxy for browser request.');
+	// 				//   return '/index.html';
+	// 				// }
+	// 			},
+	// 		},
+	// 		hot: true,
+	// 	},
 	},
 	// optimization: {
 	// 	minimize: true, // To turn on CSS optimizer in dev
