8c79a0d79e36b1d26341f1fc277d8b73160aa101
vaibhavg15 <vaibhav.goswami@goevive.com>
2022-09-23 13:38:29 +0530

    auth0 access token

--- eviveApp/Auth0Component.js
+++ eviveApp/Auth0Component.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Auth0 from 'react-native-auth0';
 import axios from "axios";
 import {TouchableOpacity, View, Text} from "react-native";
 
 const auth0Component = new Auth0({
-    domain: 'https://myevive-lite-dev.auth0.com', // TODO Check URL
+    domain: 'https://myevive-lite-dev.auth0.com',
     clientId: 'oj2lL0OJ45sS4eKZSSmK00KeI37JqzVS',
 });
 
+const api = axios.create({});
+
 const Auth0Component = () => {
+    const [accessToken, setAccessToken] = useState();
     const authorise = () => {
         auth0Component.webAuth
             .authorize({scope: 'openid email profile'})
-            .then(credentials => console.log(credentials))
+            .then(credentials => setAccessToken(credentials.accessToken))
             .catch(error => console.log(error));
     }
 
     const apiCall = async () => {
-        const apiResponse = await axios.get("https://pc.myevive.me/planchoice/api/configuration")
-        console.log(apiResponse);
+        // console.log('Api before', typeof(api));
+        console.log('Token', accessToken);
+        // api.defaults.headers.common = {'Authorization': `Bearer ${accessToken}`}
+        // console.log('Api after', api);
+        let config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            }
+        }
+        try {
+            const apiResponse = await api.get("https://pc.dev.myevive.me/planchoice/api/user-dependents/user-flow-details", config)
+            console.log(apiResponse);
+        }
+        catch (e) {
+            console.log('Error', e);
+        }
+        // const apiResponse = await api.get("https://pc.dev.myevive.me/planchoice/api/user-dependents/user-flow-details")
+        // console.log(apiResponse);
     }
 
     return (
