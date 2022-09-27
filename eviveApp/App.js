7bbe5b233a775b08daad5651935e1a1c1eb83df6
vaibhavg15 <vaibhav.goswami@goevive.com>
2022-09-27 18:14:40 +0530

    token passing from app to mfe

--- eviveApp/App.js
+++ eviveApp/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import type {Node} from 'react';
 import WebView from "react-native-webview";
 import {
@@ -21,15 +21,28 @@ import Auth0Component from "./Auth0Component";
 
 const App: () => Node = () => {
 
+    const [token, setToken] = useState('');
+
+    const onPassToken = useCallback(
+        (accessToken) => {
+            console.log('Token inside app', accessToken);
+            setToken(accessToken);
+        }, [],
+    );
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar />
-      <Image source={{uri: 'https://goevive.com/wp-content/uploads/2021/12/evive-logo.png'}} resizeMode="cover" style={{height: 100}}/>
+        <Image source={{uri: 'https://goevive.com/wp-content/uploads/2021/12/evive-logo.png'}} resizeMode="cover" style={{height: 100}}/>
         <View style={{marginTop: 32}}>
           <Text style={{fontSize: 30, fontWeight: '600', alignSelf: 'center'}}>Welcome to App POC</Text>
-          <Auth0Component />
+          <Auth0Component setToken={onPassToken}/>
         </View>
-        <WebView source={{uri: `http://localhost:8086?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlBBM2E3X1ZOVG5GS2trb0p3TTZVdCJ9.eyJodHRwczovL2V2aXZlLmlvL3VwaW4iOiJURVNUX0RJU19QTEFfVVNFUjEyIiwiaHR0cHM6Ly9ldml2ZS5pby9wZXJzb25hIjoibWVtYmVyIiwiaHR0cHM6Ly9ldml2ZS5pby9jdXN0b21lcl9pZCI6IjIwMzEiLCJpc3MiOiJodHRwczovL2FjY291bnRzLmRldi5ldml2ZS5pby8iLCJzdWIiOiJhdXRoMHxURVNUX0RJU19QTEFfVVNFUjEyIiwiYXVkIjpbImh0dHBzOi8vYXBpLnRlc3Rldml2ZWNhcmUuY29tIiwiaHR0cHM6Ly9teWV2aXZlLWxpdGUtZGV2LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2NjQyNjI5MDAsImV4cCI6MTY2NDM0OTMwMCwiYXpwIjoib2oybEwwT0o0NXNTNGVLWlNTbUswMEtlSTM3SnF6VlMiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOltdfQ.CtNUgkp7oZoTgrHhhe0cTjkwiuiN3eGMwmBYeCdzaKW_lxlhdED2nOfqnzeHLePz0ZZKbjM-8NgNccmgSs5HAfvfw1y-7pMTQmNLwCfxuvL-PNLK4aGDYZt2pKx344hrkdMBkdtOe_XlHDJo7Nb4Zh4D5we7CLXzy4XYnrBQxSQ4drN5tBc9toH9twrHGINGCBMEL1J38FkzaAbuLkMjAivYsRvkQInl3WvITcanCMPpC1nccbi_NWXQNkxYBnH6gAU-oYPEJKsXOx4HOorByQiQ8TM-Q_QAaxNo9JMaXQu3cPe24_IjcUB0E4fdtwcmnEc27tdyUeqLIQ2mRs1Nug`}} style={{flex: 1}} onMessage={() => alert('Hello')} />
+        <View style={{height: 1, backgroundColor: 'black'}} />
+        <Text style={{fontSize: 16, fontWeight: '500', alignSelf: 'center'}}>Web View 1</Text>
+        <WebView source={{uri: `http://localhost:8086?token=${token}`}} style={{flex: 1}} />
+        <View style={{height: 1, backgroundColor: 'black'}} />
+        <Text style={{fontSize: 16, fontWeight: '500', alignSelf: 'center'}}>Web View 2</Text>
+        <WebView source={{uri: `http://localhost:8086?token=${token}`}} style={{flex: 1}} />
     </SafeAreaView>
   );
 };
--- eviveApp/Auth0Component.js
+++ eviveApp/Auth0Component.js
@@ -10,13 +10,13 @@ const auth0Component = new Auth0({
 
 const api = axios.create({});
 
-const Auth0Component = () => {
+const Auth0Component = ({setToken}) => {
     const [accessToken, setAccessToken] = useState();
     const authorise = () => {
         auth0Component.webAuth
             .authorize({scope: 'openid email profile', audience: "https://api.testevivecare.com"})
             .then(credentials => {
-                console.log(JSON.stringify(credentials));
+                console.log('Access Token from auth component', JSON.stringify(credentials.accessToken));
                 setAccessToken(credentials.accessToken);
             })
             .catch(error => console.log(error));
@@ -27,6 +27,7 @@ const Auth0Component = () => {
         console.log('Token', accessToken);
         // api.defaults.headers.common = {'Authorization': `Bearer ${accessToken}`}
         // console.log('Api after', api);
+        setToken(accessToken);
         let config = {
             headers: {
                 'Content-Type': 'application/json',
@@ -49,8 +50,8 @@ const Auth0Component = () => {
             <TouchableOpacity onPress={authorise} style={{borderRadius: 8, padding: 16, backgroundColor: '#1763d3'}}>
                 <Text style={{fontSize: 20, color: 'white', fontWeight: '600'}}>Auth0 Authentication</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={apiCall} style={{borderRadius: 8, padding: 16, backgroundColor: '#1763d3'}}>
-                <Text style={{fontSize: 20, color: 'white', fontWeight: '600'}}>API Check</Text>
+            <TouchableOpacity onPress={setToken(accessToken)} style={{borderRadius: 8, padding: 16, backgroundColor: '#1763d3'}}>
+                <Text style={{fontSize: 20, color: 'white', fontWeight: '600'}}>Pass Token</Text>
             </TouchableOpacity>
         </View>
     )
