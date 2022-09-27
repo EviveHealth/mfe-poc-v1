751a06c431b987f300d327bd78310577134b1dfd
vaibhavg15 <vaibhav.goswami@goevive.com>
2022-09-27 17:41:15 +0530

    app mfe and token setup

---
+++ app-mfe/src/Benefits.js
@@ -0,0 +1,96 @@
+import React, {useEffect, useMemo, useState} from 'react';
+import queryString from 'query-string';
+import {Button, CardBase, Grid, TextBody, TextHeader} from "@evive/ui-kit";
+import axios from "axios";
+
+import './benefits.css';
+
+console.log('Location', location.search);
+const api = axios.create({});
+
+
+
+const Benefits = () => {
+
+	const [apiData, setApiData] = useState('');
+
+	const parsedUrl = queryString.parseUrl(window.location.href);
+	const accessToken = parsedUrl.query.token;
+
+	const displayData = useMemo(
+		() => {
+			if (apiData) {
+				return JSON.stringify(apiData.data);
+			}
+			return 'No Data';
+		}, [apiData],
+	);
+
+	const apiCall = async () => {
+		console.log('Token', accessToken);
+		let config = {
+			headers: {
+				// 'Content-Type': 'application/json',
+				'Authorization': `Bearer ${accessToken}`
+				// origins: '*',
+				// 'Access-Control-Allow-Origin': '*',
+				// 'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlBBM2E3X1ZOVG5GS2trb0p3TTZVdCJ9.eyJodHRwczovL2V2aXZlLmlvL3VwaW4iOiJURVNUX0RJU19QTEFfVVNFUjEyIiwiaHR0cHM6Ly9ldml2ZS5pby9wZXJzb25hIjoibWVtYmVyIiwiaHR0cHM6Ly9ldml2ZS5pby9jdXN0b21lcl9pZCI6IjIwMzEiLCJpc3MiOiJodHRwczovL2FjY291bnRzLmRldi5ldml2ZS5pby8iLCJzdWIiOiJhdXRoMHxURVNUX0RJU19QTEFfVVNFUjEyIiwiYXVkIjpbImh0dHBzOi8vYXBpLnRlc3Rldml2ZWNhcmUuY29tIiwiaHR0cHM6Ly9teWV2aXZlLWxpdGUtZGV2LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2NjQyNjI5MDAsImV4cCI6MTY2NDM0OTMwMCwiYXpwIjoib2oybEwwT0o0NXNTNGVLWlNTbUswMEtlSTM3SnF6VlMiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOltdfQ.CtNUgkp7oZoTgrHhhe0cTjkwiuiN3eGMwmBYeCdzaKW_lxlhdED2nOfqnzeHLePz0ZZKbjM-8NgNccmgSs5HAfvfw1y-7pMTQmNLwCfxuvL-PNLK4aGDYZt2pKx344hrkdMBkdtOe_XlHDJo7Nb4Zh4D5we7CLXzy4XYnrBQxSQ4drN5tBc9toH9twrHGINGCBMEL1J38FkzaAbuLkMjAivYsRvkQInl3WvITcanCMPpC1nccbi_NWXQNkxYBnH6gAU-oYPEJKsXOx4HOorByQiQ8TM-Q_QAaxNo9JMaXQu3cPe24_IjcUB0E4fdtwcmnEc27tdyUeqLIQ2mRs1Nug`
+			}
+		}
+		try {
+			const apiResponse = await api.get("http://localhost:8086/planchoice/api/user-dependents/user-flow-details?planType=MEDICAL", config)
+			console.log('API Response', apiResponse);
+			setApiData(apiResponse);
+		}
+		catch (e) {
+			console.log('Error', e);
+		}
+		// const apiResponse = await api.get("https://pc.dev.myevive.me/planchoice/api/user-dependents/user-flow-details")
+		// console.log(apiResponse);
+	}
+
+	return (
+		<div>
+			<div>Benefits</div>
+			{/*<div>{JSON.stringify(parsedUrl.query.token)}</div>*/}
+			<div>{displayData}</div>
+			<Grid container>
+				<Grid colSpan={3}>
+					<CardBase>
+						<div>
+							<TextHeader>Card 1</TextHeader>
+							<TextBody>Description</TextBody>
+						</div>
+					</CardBase>
+				</Grid>
+				<Grid colSpan={3}>
+					<CardBase>
+						<div>
+							<TextHeader>Card 1</TextHeader>
+							<TextBody>Description</TextBody>
+						</div>
+					</CardBase>
+				</Grid>
+				<Grid colSpan={3}>
+					<CardBase>
+						<div>
+							<TextHeader>Card 1</TextHeader>
+							<TextBody>Description</TextBody>
+						</div>
+					</CardBase>
+				</Grid>
+				<Grid colSpan={3}>
+					<CardBase>
+						<div>
+							<TextHeader>Card 1</TextHeader>
+							<TextBody>Description</TextBody>
+						</div>
+					</CardBase>
+				</Grid>
+			</Grid>
+			<Button onClickHandler={apiCall}>API CALL</Button>
+		</div>
+	);
+}
+
+export default Benefits;
--- eviveApp/App.js
+++ eviveApp/App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import Auth0Component from "./Auth0Component";
+import WebView from "react-native-webview";
 import {
   Image,
   SafeAreaView,
@@ -17,6 +17,7 @@ import {
   View,
   Text,
 } from 'react-native';
+import Auth0Component from "./Auth0Component";
 
 const App: () => Node = () => {
 
@@ -24,12 +25,11 @@ const App: () => Node = () => {
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar />
       <Image source={{uri: 'https://goevive.com/wp-content/uploads/2021/12/evive-logo.png'}} resizeMode="cover" style={{height: 100}}/>
-      <ScrollView>
         <View style={{marginTop: 32}}>
           <Text style={{fontSize: 30, fontWeight: '600', alignSelf: 'center'}}>Welcome to App POC</Text>
           <Auth0Component />
         </View>
-      </ScrollView>
+        <WebView source={{uri: `http://localhost:8086?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlBBM2E3X1ZOVG5GS2trb0p3TTZVdCJ9.eyJodHRwczovL2V2aXZlLmlvL3VwaW4iOiJURVNUX0RJU19QTEFfVVNFUjEyIiwiaHR0cHM6Ly9ldml2ZS5pby9wZXJzb25hIjoibWVtYmVyIiwiaHR0cHM6Ly9ldml2ZS5pby9jdXN0b21lcl9pZCI6IjIwMzEiLCJpc3MiOiJodHRwczovL2FjY291bnRzLmRldi5ldml2ZS5pby8iLCJzdWIiOiJhdXRoMHxURVNUX0RJU19QTEFfVVNFUjEyIiwiYXVkIjpbImh0dHBzOi8vYXBpLnRlc3Rldml2ZWNhcmUuY29tIiwiaHR0cHM6Ly9teWV2aXZlLWxpdGUtZGV2LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2NjQyNjI5MDAsImV4cCI6MTY2NDM0OTMwMCwiYXpwIjoib2oybEwwT0o0NXNTNGVLWlNTbUswMEtlSTM3SnF6VlMiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOltdfQ.CtNUgkp7oZoTgrHhhe0cTjkwiuiN3eGMwmBYeCdzaKW_lxlhdED2nOfqnzeHLePz0ZZKbjM-8NgNccmgSs5HAfvfw1y-7pMTQmNLwCfxuvL-PNLK4aGDYZt2pKx344hrkdMBkdtOe_XlHDJo7Nb4Zh4D5we7CLXzy4XYnrBQxSQ4drN5tBc9toH9twrHGINGCBMEL1J38FkzaAbuLkMjAivYsRvkQInl3WvITcanCMPpC1nccbi_NWXQNkxYBnH6gAU-oYPEJKsXOx4HOorByQiQ8TM-Q_QAaxNo9JMaXQu3cPe24_IjcUB0E4fdtwcmnEc27tdyUeqLIQ2mRs1Nug`}} style={{flex: 1}} onMessage={() => alert('Hello')} />
     </SafeAreaView>
   );
 };
